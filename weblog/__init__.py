eb38a3e2ebf7ace0a27b048883d5f0445711fb92
vincent <vincent.wyshan@gmail.com>
2013-08-17 00:53:53 +0800

    modified:   weblog/__init__.py 	modified:   weblog/templates/blog_about.mako

--- weblog/__init__.py
+++ weblog/__init__.py
@@ -15,7 +15,7 @@ def main(global_config, **settings):
     Base.metadata.bind = engine
     set_cache_regions_from_settings(settings)
     config = Configurator(settings=settings)
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'static', cache_max_age=36000)
     config.add_route('blog_home', '/')
 
     config.add_route('blog_post', '/post/{kword_or_id}')
--- weblog/templates/blog_about.mako
+++ weblog/templates/blog_about.mako
@@ -1,5 +1,9 @@
 <%inherit file="blog_base.mako" />
 
+<%block name="meta_head">
+<title>About</title>
+</%block>
+
 <%block name="_body">
 
 <div>
