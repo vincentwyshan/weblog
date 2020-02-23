79268e1b8062410cd40299bb5b92bc9f720523da
Vincent Wen <vincent.wyshan@gmail.com>
2020-02-23 18:09:02 +0800

    remove padding in tags.html

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ class CustomCommand(distutils.cmd.Command):
 
 setup(
     name="weblog",
-    version="0.3.9",
+    version="0.3.91",
     description="weblog",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
