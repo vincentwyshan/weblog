a5ff41209b0c4e6311e27ee3b5de3ae0a821755c
Vincent Wen <vincent.wyshan@gmail.com>
2020-02-23 13:37:42 +0800

    upgade version number

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ class CustomCommand(distutils.cmd.Command):
 
 setup(
     name="weblog",
-    version="0.3.4",
+    version="0.3.5",
     description="weblog",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
